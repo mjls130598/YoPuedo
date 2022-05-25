9730739282881102460e841843087d9a73a0b564
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-05-26 01:01:39 +0200

    fix: Modificando los modelos para asignar a los campos correspondientes como clave primaria (2)

--- TFM/MisRetos/models.py
+++ TFM/MisRetos/models.py
@@ -11,8 +11,8 @@ class Usuario(models.Model):
 
 # TABLA MisRetos_amistad
 class Amistad(models.Model):
-    amigo = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='amigo', primary_key=True)
-    otro_amigo = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='otro_amigo', primary_key=True)
+    amigo = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, related_name='amigo', primary_key=True)
+    otro_amigo = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, related_name='otro_amigo', primary_key=True)
 
     class Meta:
         unique_together = (('amigo', 'otro_amigo'),)
@@ -25,13 +25,13 @@ class Reto(models.Model):
     recompensa = models.CharField(max_length=500)
     estado = models.CharField(max_length=10, default="Propuesto")
     categoria = models.CharField(max_length=15)
-    coordinador = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column="email")
+    coordinador = models.ManyToOneRel(Usuario, on_delete=models.CASCADE, db_column="email")
 
 
 # TABLA MisRetos_participante:
 class Participante(models.Model):
-    email = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
-    id_reto = models.ForeignKey(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
+    email = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
+    id_reto = models.ManyToManyRel(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
 
     class Meta:
         unique_together = (('email', 'id_reto'),)
@@ -39,8 +39,8 @@ class Participante(models.Model):
 
 # TABLA MisRetos_animador:
 class Animador(models.Model):
-    email = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
-    id_reto = models.ForeignKey(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
+    email = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
+    id_reto = models.ManyToManyRel(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
     superanimador = models.BooleanField(default=False)
 
     class Meta:
@@ -50,29 +50,29 @@ class Animador(models.Model):
 # TABLA MisRetos_etapa
 class Etapa(models.Model):
     id_etapa = models.CharField(max_length=50, primary_key=True)
-    id_reto = models.ForeignKey(Reto, on_delete=models.CASCADE, db_column="id_reto")
+    id_reto = models.ManyToOneRel(Reto, on_delete=models.CASCADE, db_column="id_reto")
     objetivo = models.CharField(max_length=500)
     estado = models.CharField(max_length=10, default="Propuesto")
 
 
 # TABLA MisRetos_animo
 class Animo(models.Model):
-    id_etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
-    animador = models.ForeignKey(Animador, on_delete=models.CASCADE, db_column="email")
+    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
+    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email")
     mensaje = models.CharField(max_length=500)
 
 
 # TABLA MisRetos_prueba
 class Prueba(models.Model):
-    id_etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
-    animador = models.ForeignKey(Animador, on_delete=models.CASCADE, db_column="email")
+    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
+    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email")
     prueba = models.CharField(max_length=500)
 
 
 # TABLA MisRetos_calificacion
 class Calificacion(models.Model):
-    id_etapa = models.ForeignKey(Etapa, on_delete=models.CASCADE, db_column="id_etapa", primary_key=True)
-    animador = models.ForeignKey(Animador, on_delete=models.CASCADE, db_column="email", primary_key=True)
+    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa", primary_key=True)
+    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email", primary_key=True)
     prueba = models.CharField(max_length=500)
 
     class Meta:
@@ -85,4 +85,4 @@ class Notificacion(models.Model):
     mensaje = models.CharField(max_length=500)
     categoria = models.CharField(max_length=15)
     estado = models.CharField(max_length=10, default="Recibido")
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, db_column="email")
+    usuario = models.ManyToOneRel(Usuario, on_delete=models.CASCADE, db_column="email")
