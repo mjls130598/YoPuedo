28011fb2dc5290d97b8ff23b755428883194a65a
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-05-26 01:04:50 +0200

    fix: Modificando los modelos para asignar a los campos correspondientes como clave primaria (3)

--- TFM/MisRetos/models.py
+++ TFM/MisRetos/models.py
@@ -11,11 +11,8 @@ class Usuario(models.Model):
 
 # TABLA MisRetos_amistad
 class Amistad(models.Model):
-    amigo = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, related_name='amigo', primary_key=True)
-    otro_amigo = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, related_name='otro_amigo', primary_key=True)
-
-    class Meta:
-        unique_together = (('amigo', 'otro_amigo'),)
+    amigo = models.ManyToManyRel(Usuario)
+    otro_amigo = models.ManyToManyRel(Usuario)
 
 
 # TABLA MisRetos_reto
@@ -25,58 +22,49 @@ class Reto(models.Model):
     recompensa = models.CharField(max_length=500)
     estado = models.CharField(max_length=10, default="Propuesto")
     categoria = models.CharField(max_length=15)
-    coordinador = models.ManyToOneRel(Usuario, on_delete=models.CASCADE, db_column="email")
+    coordinador = models.ManyToOneRel(Usuario)
 
 
 # TABLA MisRetos_participante:
 class Participante(models.Model):
-    email = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
-    id_reto = models.ManyToManyRel(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
-
-    class Meta:
-        unique_together = (('email', 'id_reto'),)
+    email = models.ManyToManyRel(Usuario)
+    id_reto = models.ManyToManyRel(Reto)
 
 
 # TABLA MisRetos_animador:
 class Animador(models.Model):
-    email = models.ManyToManyRel(Usuario, on_delete=models.CASCADE, db_column="email", primary_key=True)
-    id_reto = models.ManyToManyRel(Reto, on_delete=models.CASCADE, db_column="id_reto", primary_key=True)
+    email = models.ManyToManyRel(Usuario)
+    id_reto = models.ManyToManyRel(Reto)
     superanimador = models.BooleanField(default=False)
-
-    class Meta:
-        unique_together = (('email', 'id_reto'),)
 
 
 # TABLA MisRetos_etapa
 class Etapa(models.Model):
     id_etapa = models.CharField(max_length=50, primary_key=True)
-    id_reto = models.ManyToOneRel(Reto, on_delete=models.CASCADE, db_column="id_reto")
+    id_reto = models.ManyToOneRel(Reto)
     objetivo = models.CharField(max_length=500)
     estado = models.CharField(max_length=10, default="Propuesto")
 
 
 # TABLA MisRetos_animo
 class Animo(models.Model):
-    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
-    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email")
+    id_etapa = models.ManyToManyRel(Etapa)
+    animador = models.ManyToManyRel(Animador)
     mensaje = models.CharField(max_length=500)
 
 
 # TABLA MisRetos_prueba
 class Prueba(models.Model):
-    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa")
-    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email")
+    id_etapa = models.ManyToManyRel(Etapa)
+    animador = models.ManyToManyRel(Animador)
     prueba = models.CharField(max_length=500)
 
 
 # TABLA MisRetos_calificacion
 class Calificacion(models.Model):
-    id_etapa = models.ManyToManyRel(Etapa, on_delete=models.CASCADE, db_column="id_etapa", primary_key=True)
-    animador = models.ManyToManyRel(Animador, on_delete=models.CASCADE, db_column="email", primary_key=True)
+    id_etapa = models.ManyToManyRel(Etapa)
+    animador = models.ManyToManyRel(Animador)
     prueba = models.CharField(max_length=500)
-
-    class Meta:
-        unique_together = (('id_etapa', 'animador'),)
 
 
 # TABLA MisRetos_notificacion:
@@ -85,4 +73,4 @@ class Notificacion(models.Model):
     mensaje = models.CharField(max_length=500)
     categoria = models.CharField(max_length=15)
     estado = models.CharField(max_length=10, default="Recibido")
-    usuario = models.ManyToOneRel(Usuario, on_delete=models.CASCADE, db_column="email")
+    usuario = models.ManyToOneRel(Usuario)
