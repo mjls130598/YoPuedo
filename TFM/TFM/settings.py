f1a8071034c037f31236a613439abdda804aaabd
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-05-25 18:30:05 +0200

    fix: Corrección del secret_key

--- TFM/TFM/settings.py
+++ TFM/TFM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = open('conf/.secret_key', 'r', encoding='utf-8').read()
+SECRET_KEY = open(os.path.join(BASE_DIR, 'conf/.secret_key'), 'r', encoding='utf-8').read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
