1ecbacd449686ce8bac9d1e063cd764263ba02f5
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-05-29 13:04:31 +0200

    fix: Arrglamos la configuración de la app

--- TFM/YoPuedo/apps.py
+++ TFM/YoPuedo/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MisretosConfig(AppConfig):
+class YopuedoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MisRetos'
+    name = 'YoPuedo'
