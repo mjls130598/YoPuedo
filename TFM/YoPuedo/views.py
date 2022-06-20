7e01caf746e918672fb93bfb0ce428524f250c4c
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-06-20 19:07:07 +0200

    fix: CAmbio del nombre del formulario del registro #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -5,4 +5,4 @@ from .forms import registro
 def registrarse(request):
     if request.method == 'GET':
         form = registro()
-        return render(request, "YoPuedo/registro.html", {'form': form})
+        return render(request, "YoPuedo/registro.html", {'register_form': form})
