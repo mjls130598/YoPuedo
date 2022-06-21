ea28678e063711bb560ef5f3aa93d3d21645f5cd
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-06-21 18:05:18 +0200

    feat: Añadimos logs al código (configuración)

--- TFM/TFM/settings.py
+++ TFM/TFM/settings.py
@@ -142,3 +142,50 @@ PASSWORD_HASHERS = [
 ]
 
 LOGOUT_REDIRECT_URL = '/registrarse/'
+LOGIN_REDIRECT_URL = '/registrarse/'
+
+LOG_FILE = os.path.join(BASE_DIR, 'Server.log')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s [%(name)s:%(lineno)s] %(message)s'
+        },
+    },
+
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, LOG_FILE),
+            'formatter': 'verbose',
+            'mode': 'w'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'ERROR',
+        },
+        '': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+        }
+    }
+}
+
+ROOT_URLCONF = 'YoPuedo.urls'
