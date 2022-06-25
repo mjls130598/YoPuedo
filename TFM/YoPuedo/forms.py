7f59815a84bfcad0a3ce7766fa3705c6e9606edf
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-06-25 12:16:59 +0200

    style: Arreglamos la visualización campos contraseña #1

--- TFM/YoPuedo/forms.py
+++ TFM/YoPuedo/forms.py
@@ -22,12 +22,12 @@ class Registro(forms.Form):
     password = forms.CharField(label='Contraseña:', max_length='16', min_length='8',
                                widget=forms.PasswordInput(
                                    attrs={
-                                       'class': 'form-control'
+                                       'class': 'col-12'
                                    }))
     password_again = forms.CharField(label='Repetir contraseña:', max_length='16',
                                      min_length='8', widget=forms.PasswordInput(
             attrs={
-                'class': 'form-control'
+                'class': 'col-12'
             }))
     foto_de_perfil = forms.ImageField(label='Foto de perfil:',
                                       widget=forms.ClearableFileInput(
