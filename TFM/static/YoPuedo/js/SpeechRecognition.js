dca0dbadca1bb7f53368fd28cc527b1b95c7b0d8
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-06-26 19:16:21 +0200

    feat: Incorporamos el reconocimiento al registro #1

--- TFM/static/YoPuedo/js/SpeechRecognition.js
+++ TFM/static/YoPuedo/js/SpeechRecognition.js
@@ -1,5 +1,11 @@
 // MÉTODO QUE REALIZA EL RECONOCIMIENTO DE VOZ
 
+// Variable para guardar el ID del input donde va a escribir
+var inputSpeech = "";
+
+// Variable booleana que indica si está activo o no el speech recognition
+var encendido = false;
+
 // 1º Comprobamos que se puede lanzar en ese navegador/dispositivo
 if (!('webkitSpeechRecognition' in window)) {
     alert("No puede utilizar el micrófono en este navegador o dispositivo.");
@@ -26,28 +32,43 @@ recognition.onend = function() {
     console.log('Termina el reconocimiento de voz');
 };
 
+// 6º Creamos las acciones que se deben realizar cuando se recibe un resultado
+recognition.onresult = function(event) {
+    var interim_transcript = '';
+    var final_transcript = '';
+
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+        // Verify if the recognized text is the last with the isFinal property
+        if (event.results[i].isFinal) {
+            final_transcript += event.results[i][0].transcript;
+        } else {
+            interim_transcript += event.results[i][0].transcript;
+        }
+    }
+
+    // Choose which result may be useful for you
+
+    console.log("Interim: ", interim_transcript);
+
+    console.log("Final: ",final_transcript);
+
+    console.log("Simple: ", event.results[0][0].transcript);
+
+    document.getElementById(inputSpeech).value = final_transcript;
+};
+
+// Método que activa/apada el reconocimiento de voz desde el HTML
 function reconocimientoVoz(input){
 
-    // 6º Creamos las acciones que se deben realizar cuando se recibe un resultado
-    recognition.onresult = function(event) {
-        var interim_transcript = '';
-        var final_transcript = '';
+    inputSpeech = input;
 
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            // Verify if the recognized text is the last with the isFinal property
-            if (event.results[i].isFinal) {
-                final_transcript += event.results[i][0].transcript;
-            } else {
-                interim_transcript += event.results[i][0].transcript;
-            }
-        }
+    if(encendido){
+        console.log("Apagamos reconocimiento");
+        recognition.stop();
+    }
 
-        // Choose which result may be useful for you
-
-        console.log("Interim: ", interim_transcript);
-
-        console.log("Final: ",final_transcript);
-
-        console.log("Simple: ", event.results[0][0].transcript);
-    };
+    else{
+        console.log("Activamos reconocimiento");
+        recognition.start();
+    }
 }
