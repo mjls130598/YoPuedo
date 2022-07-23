790e4bbe4ce83af3e1ac67b4a02e75499374405c
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-07-23 12:51:05 +0200

    fix: Añadimos tabulador al mensaje de error contraseña #2

--- TFM/YoPuedo/forms.py
+++ TFM/YoPuedo/forms.py
@@ -114,7 +114,7 @@ class InicioForm(forms.Form):
             usuario = Usuario.objects.get(email=email)
             if usuario.check_password(password):
                 logger.error("Contraseña incorrecta")
-            self.add_error('password_sesion', "Usuario y/o contraseña incorrect@")
+                self.add_error('password_sesion', "Usuario y/o contraseña incorrect@")
 
         return self
 
