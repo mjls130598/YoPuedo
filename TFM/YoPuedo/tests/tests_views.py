591f1aad85306aae835f9c85c9f646102a5b3192
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-07-23 14:04:41 +0200

    test: Quitamos validar clave en views #1

--- TFM/YoPuedo/tests/tests_views.py
+++ TFM/YoPuedo/tests/tests_views.py
@@ -6,13 +6,6 @@ from ..models import Usuario
 
 # Comprobamos el funcionamiento de la URL registrarse
 class RegistroViewTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        Usuario.objects.create(email="mj@gmail.com", nombre="María Jesús",
-                               password="Password1.",
-                               fotoPerfil=f"{BASE_DIR}/media/YoPuedo/foto_perfil/mariajesus@gmail.com.jpg",
-                               claveFija="clavefija",
-                               claveAleatoria="clavefija")
 
     def test_url_accesible(self):
         resp = self.client.get('/registrarse/')
@@ -28,25 +21,6 @@ class RegistroViewTest(TestCase):
         }
         resp = self.client.post('/registrarse/', data)
         self.assertEqual(resp.status_code, HTTPStatus.OK)
+        self.assertEqual(resp.context['user'].is_active)
 
-    def test_validar_clave(self):
-        data = {
-            'email': "mj@gmail.com",
-            'contador': 0,
-            'tipo': 'registro',
-            'clave': 'clavefija'
-        }
 
-        resp = self.client.post('/validar_clave/', data)
-        self.assertEqual(resp.status_code, HTTPStatus.OK)
-
-    def test_validar_clave_incorrecta(self):
-        data = {
-            'email': "mj@gmail.com",
-            'contador': 0,
-            'tipo': 'registro',
-            'clave': 'clave'
-        }
-
-        resp = self.client.post('/validar_clave/', data)
-        self.assertEqual(resp.path, '/validar_clave/')
