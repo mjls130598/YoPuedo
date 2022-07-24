c696f661c902c4c94d2f043190683367e59c7ab4
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-07-24 12:46:49 +0200

    fix: Modificamos password hashers

--- TFM/TFM/settings.py
+++ TFM/TFM/settings.py
@@ -140,11 +140,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password Hashers
 PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.ScryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.ScryptPasswordHasher',
 ]
 
 LOGOUT_REDIRECT_URL = '/registrarse/'
