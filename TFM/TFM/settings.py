1c8e3af81ed743de6283caad90ff774288141066
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-07-24 11:25:09 +0200

    fix: Corregimos problemas con los hashers

--- TFM/TFM/settings.py
+++ TFM/TFM/settings.py
@@ -140,9 +140,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Password Hashers
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.ScryptPasswordHasher',
 ]
