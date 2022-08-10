612778c3d8430cd572a51ed131056b897e058245
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-10 19:24:48 +0200

    fix: Modificamos URL clave en template #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -44,8 +44,7 @@ def registrarse(request):
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
             return HttpResponse(status=HTTPStatus.CREATED,
-                                headers={'HX-Trigger': 'postRegistro'},
-                                json_data={'email': email})
+                                headers={'HX-Trigger': 'postClave'})
         else:
             logger.error("Error al validar el formulario")
 
