72e6fb1e9e53b499181535fc86610426af32f93e
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-10 19:09:08 +0200

    fix: Corregimos URL clave en registro parte views #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -44,7 +44,8 @@ def registrarse(request):
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
             return HttpResponse(status=HTTPStatus.CREATED,
-                                headers={'HX-Trigger': 'postRegistro'})
+                                headers={'HX-Trigger': 'postRegistro'},
+                                json_data={'email': email})
         else:
             logger.error("Error al validar el formulario")
 
