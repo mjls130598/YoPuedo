5cd443289b5892b28f062d0dffbeffc4c3705450
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-11 20:38:23 +0200

    fix: Activamos manualmente modal registro #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -43,7 +43,8 @@ def registrarse(request):
 
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
-            return redirect(f'/validar_clave/registro/{email}')
+            return render(request, "YoPuedo/registro.html",
+                          {'register_form': form, 'url': f'/validar_clave/registro/{email}'})
 
         else:
             logger.error("Error al validar el formulario")
