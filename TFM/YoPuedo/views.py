0af611bee20b049cb94daf46d7cd1f8ecae58435
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-11 18:32:17 +0200

    fix: Añadimos respuesta #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -43,7 +43,8 @@ def registrarse(request):
 
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
-            return HttpResponse(headers={'HX-Trigger': 'postClave'})
+            return HttpResponse(status=HTTPStatus.CREATED,
+                                headers={'HX-Trigger': 'postClave'})
         else:
             logger.error("Error al validar el formulario")
 
