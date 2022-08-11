6390ca89be5e986cd22106c182750c63124e9766
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-11 19:15:33 +0200

    fix: Cambiamos HttpResponse por render #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -43,8 +43,11 @@ def registrarse(request):
 
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
-            return HttpResponse(status=HTTPStatus.CREATED,
-                                headers={'HX-Trigger': 'postClave'})
+            response = render(request, "YoPuedo/registro.html", {'register_form': form},
+                          status=HTTPStatus.CREATED)
+
+            response.headers = {'HX-Trigger': 'postRegistro'}
+            return response
         else:
             logger.error("Error al validar el formulario")
 
