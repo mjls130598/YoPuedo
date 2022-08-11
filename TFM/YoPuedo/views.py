e74762d7f2909ff12a96dbd76cd2920b92b0e920
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-08-11 19:31:49 +0200

    fix: Quitamos render #1

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -43,10 +43,8 @@ def registrarse(request):
 
             login(request, usuario, backend='django.contrib.auth.backends.ModelBackend')
 
-            response = render(request, "YoPuedo/registro.html", {'register_form': form})
+            return HttpResponse(status=HTTPStatus.CREATED)
 
-            response.headers = {'HX-Trigger': 'postRegistro'}
-            return response
         else:
             logger.error("Error al validar el formulario")
 
