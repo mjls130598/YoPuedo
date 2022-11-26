272c098aa295c292788f8d93d09db986de31bdc7
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-11-26 13:40:23 +0100

    fix: Quitamos required de la consulta #21

--- TFM/YoPuedo/forms.py
+++ TFM/YoPuedo/forms.py
@@ -331,3 +331,7 @@ class AmigosForm(forms.Form):
             'class': 'form-control',
             'placeholder': 'Buscar amigo ...'
         }))
+
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.fields['consulta'].required = False
