02901cc2a9c2c8a6c207918e5bff06ea9ce305be
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2022-12-17 18:07:41 +0100

    test: Eliminar clases importadas innecesarias

--- TFM/YoPuedo/tests/tests_models.py
+++ TFM/YoPuedo/tests/tests_models.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from ..models import Usuario, Reto, Etapa, Animador, Participante
-from ..utils import Utils
 
 
 # Comprobamos el funcionamiento de la tabla USUARIO
