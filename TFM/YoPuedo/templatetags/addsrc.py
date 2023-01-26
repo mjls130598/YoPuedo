6fdd8203b468f520213d46e8cca4450a6adb99a6
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-01-26 08:17:26 +0100

    fix: Cambiamos filter por simple_tag #35

--- TFM/YoPuedo/templatetags/addsrc.py
+++ TFM/YoPuedo/templatetags/addsrc.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='addsrc')
+@register.simple_tag
 def addsrc(field, src):
     return field.as_widget(attrs={"src": src})
