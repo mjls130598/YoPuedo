0f8cacc30ce2c1f37199445f13d5ae3662019344
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-01-27 07:40:49 +0100

    fix: Modificamos el lugar de llamar al tag en template #35

--- TFM/YoPuedo/templatetags/addsrc.py
+++ TFM/YoPuedo/templatetags/addsrc.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.filter
+@register.filter(name='addsrc')
 def addsrc(field, src):
     return field.as_widget(attrs={"src": src})
