087fc6680158410f824afda621ad6b8bc1716b94
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-02-04 12:46:16 +0100

    fix: Añadimos filter #35

--- TFM/YoPuedo/templatetags/get_value.py
+++ TFM/YoPuedo/templatetags/get_value.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
+@register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key)
