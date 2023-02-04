d43d4e34ae8e49f11b70346a05d6da3ad7b24d90
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-02-04 13:42:17 +0100

    fix: Corregimos form-MAX_NUM_FORMS en data #35

--- TFM/YoPuedo/views.py
+++ TFM/YoPuedo/views.py
@@ -812,6 +812,7 @@ def editar_reto(request, id_reto):
                 # Etapas
                 'form-INITIAL_FORMS': '0',
                 'form-TOTAL_FORMS': len(etapas),
+                'form-MAX_NUM_FORMS': max_etapas,
             }
 
             for index, etapa in enumerate(etapas):
