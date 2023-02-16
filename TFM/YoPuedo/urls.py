f6cfcbce4f3ebc0efa5ef90514b67b28138e42de
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-02-16 08:15:48 +0100

    fix: Añadimos > en la url calificar #30

--- TFM/YoPuedo/urls.py
+++ TFM/YoPuedo/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
          name="coordinador_reto"),
     path('animador_reto/<str:id_reto>', views.animador_reto, name="animador_reto"),
     path('amigos/', views.get_amigos, name="amigos"),
-    path('calificar/<str:id_etapa', views.calificar_etapa, name="calificar")
+    path('calificar/<str:id_etapa>', views.calificar_etapa, name="calificar")
 ]
