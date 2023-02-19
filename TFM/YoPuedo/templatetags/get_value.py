0f3a83580ad952f9eb66e15be87c79af4fdb350e
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-02-19 13:35:47 +0100

    fix: Creamos función de concatenación de cadenas #14

--- TFM/YoPuedo/templatetags/get_value.py
+++ TFM/YoPuedo/templatetags/get_value.py
@@ -2,7 +2,12 @@ from django import template
 
 register = template.Library()
 
-
+# Función obtención de valor de un diccionario
 @register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key) if key in dictionary else ""
+
+# Función concatenación de strings
+@register.filter(name='addvalue')
+def add_value(value1, value2):
+    return str(value1) + str(value2)
