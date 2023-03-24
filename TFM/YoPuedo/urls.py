ad43b11a54a6d59c80538463999123491e94a40d
Maria Jesus Lopez Salmeron <mjls130598@gmail.com>
2023-03-24 07:52:34 +0100

    fix: Cambiamos '/' por ''

--- TFM/YoPuedo/urls.py
+++ TFM/YoPuedo/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('registrarse/', views.registrarse, name="registrarse"),
     path('validar_clave/<str:tipo>/<str:email>', views.validar_clave,
          name="validar_clave"),
